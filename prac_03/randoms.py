7891f076d902e7655e2229e193c278ac50f2757e
Iestyn Townsend <iestyn.townsend@my.jcu.edu.au>
2023-10-01 19:39:03 +1000

    finished randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,8 @@
+# 1. I saw number 17. Largest possible is 20 and smallest is 1
+
+# 2. I saw 9, smallest possible is 3 and largest is 9. Line 2 could not produce 4 it can only do 3, 5 or 9
+
+# 3. I saw 4.129256814574966, smallest possible is 2.5 and largest is 5.5
+
+import random
+print(random.randint(1, 100))

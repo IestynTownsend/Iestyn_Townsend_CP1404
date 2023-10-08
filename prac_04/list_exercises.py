8cb265c82e32c06c801771f46b29631ad09a2ffc
Iestyn Townsend <iestyn.townsend@my.jcu.edu.au>
2023-10-08 14:33:30 +1000

    Requests teh 5 numbers from the user

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,7 @@
+# creates an empty list to store the numbers in
+numbers = []
+
+# prompt for the user to enter 5 numbers
+for i in range(5):
+    number = float(input("Number: "))
+    numbers.append(number)

8bb54a89a3a4bec6704034613ef463f337b3169d
Iestyn Townsend <iestyn.townsend@my.jcu.edu.au>
2023-10-08 14:37:12 +1000

    finished list_exercises.py file

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,3 +5,12 @@ numbers = []
 for i in range(5):
     number = float(input("Number: "))
     numbers.append(number)
+
+# output the required information about the numbers
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+average = sum(numbers) / len(numbers)
+print(f"The average of the numbers is {average:.1f}")
+# unsure if having a ".0" at the end of the first 4 of ok, if not a ":.0f" should be appended.

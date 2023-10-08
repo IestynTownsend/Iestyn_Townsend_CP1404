7c4641d8a48986dcc3ab33514915dbb199717136
Iestyn Townsend <iestyn.townsend@my.jcu.edu.au>
2023-10-08 14:30:23 +1000

    completed question 3 which finished off the program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,13 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_subject_information(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    data = [] # Initialise an empty list to store the data
+    data = []  # Initialise an empty list to store the data
     for line in input_file:
         line = line.strip()
         parts = line.split(',')
@@ -22,6 +22,13 @@ def get_data():
         data.append(parts)  # Append the parts to the data list
     input_file.close()
     return data
+
+
+def display_subject_information(data):
+    for subject in data:
+        # unsure why "= subject" needs to be at the back, but it does not work otherwise.
+        subject_code, lecturer, number_of_students = subject
+        print(f"{subject_code} is taught by {lecturer} and has {number_of_students} enrolled.")
 
 
 main()
